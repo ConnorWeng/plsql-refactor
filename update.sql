95001c7b9d1e6f31107105aad7a5e4d47d2ad25c
KFZX-wangyang01 <wangyang@sdc.icbc.com.cn>
2014-01-15 11:12:11 +0800

    extract view v_invest_term_acct_emp_and_co

--- update.sql
+++ update.sql
@@ -23,3 +23,13 @@ comment on column DEMO_INVEST_POP_TMP.quotient_remain
 
 -- Drop columns 
 alter table DEMO_INVEST_POP_TMP drop column amt_remain;
+
+create OR REPLACE view v_invest_term_acct_emp_and_co as
+SELECT INVEST_TIME, CO_ID, invest_id,emp_id,subject_type,amt
+  FROM DEMO_EMP_INVEST_TERM
+ WHERE AMT > 0
+UNION
+SELECT INVEST_TIME, CO_ID, invest_id,'FFFFFFFFFF' emp_id, subject_type,amt
+  FROM DEMO_co_INVEST_TERM
+ WHERE AMT > 0
+   and subject_type >= '302001';
